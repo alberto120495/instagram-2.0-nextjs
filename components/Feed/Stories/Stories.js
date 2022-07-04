397c9fd62fec84e556d5c0342eaf4a940ca88e3c
Alberto <albertopimentel04@gmail.com>
2022-07-04 17:49:53 -0500

    Correccion de libreria faker

--- components/Feed/Stories/Stories.js
+++ components/Feed/Stories/Stories.js
@@ -1,21 +1,18 @@
-import faker from "faker";
+/* import faker from "faker"; */
 import { useEffect, useState } from "react";
 import Story from "./Story";
 import { useSession } from "next-auth/react";
+import { faker } from "../../../data/faker";
 
 function Stories() {
   const { data: session } = useSession();
 
   const [suggestions, setSuggestions] = useState([]);
   useEffect(() => {
-    const suggestions = [...Array(20)].map((_, i) => {
-      return {
-        ...faker.helpers.contextualCard(),
-        id: i,
-      };
-    });
+    const suggestions = faker;
     setSuggestions(suggestions);
   }, []);
+
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border border-gray-200 rounded-sm overflow-x-scroll scrollbar-thin  scrollbar-thumb-black">
       {session && (
